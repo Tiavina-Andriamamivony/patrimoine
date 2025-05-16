1230fdb3a31c735b3f0e80fe87a69ec83f4bb547
Tiavina-Andriamamivony <hei.tiavina.1@gmail.com>
2025-05-16 08:14:24 +0300

    feat(objet): Calcul de la valeur de l'objet a une date donnée.

--- src/main/java/org/prog/models/Objet.java
+++ src/main/java/org/prog/models/Objet.java
@@ -1,18 +1,22 @@
 package org.prog.models;
 
+import java.time.LocalDate;
+
 public non-sealed class Objet extends Possession{
     private Double tauxAmmortissement;
+    private LocalDate dateAchat;
 
-    public Objet(Utilisateur utilisateur, ValeurMonetaire valeur, Double tauxAmmortissement) {
-        super(utilisateur, valeur);
-        this.tauxAmmortissement = tauxAmmortissement;
-    }
 
-    public Objet() {
-    }
+    public Double ValeurADateDe(LocalDate dateVise) {
+        int differenceYear = dateVise.getYear() - dateAchat.getYear();
 
-    public Double getTauxAmmortissement() {
-        return tauxAmmortissement;
+
+        if (differenceYear < 0) return 0.0;
+
+        Double decrementation = (this.getValeurs().getValeur()*(tauxAmmortissement*differenceYear))/100;
+
+        return this.getValeurs().getValeur() - decrementation;
+
     }
 
 }
